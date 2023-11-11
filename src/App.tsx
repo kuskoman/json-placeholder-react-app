3dc3c0c8005d37d357fbefec8bc3e5f0b5783988
Jakub <kubasurdej@gmail.com>
2023-11-11 22:29:05 +0100

    feat: add broken footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "./App.css";
+import { AppBar, Toolbar, Typography, Container, Button, Box } from "@mui/material";
 import { RegisterPage } from "./page/Register/Register";
+import { Footer } from "./components/Common/Footer/Footer";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/register">Register</Link>
-        </nav>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My App
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/register">
+            Register
+          </Button>
+        </Toolbar>
+      </AppBar>
 
-        <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </div>
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <Container maxWidth="md" sx={{ mt: 4 }}>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </Box>
     </Router>
   );
 }
---
+++ src/components/Common/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+// src/components/Common/Footer.tsx
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+
+export const Footer: React.FC = () => {
+  return (
+    <Box component="footer" sx={{ bgcolor: "primary.main", color: "primary.contrastText", py: 3 }}>
+      <Container maxWidth="lg">
+        <Typography variant="body1">My React App Footer</Typography>
+        <Typography variant="body2" sx={{ color: "primary.contrastText" }}>
+          © {new Date().getFullYear()} My React App
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
