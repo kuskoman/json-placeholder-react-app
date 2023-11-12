ec2aabe88206835438ca1ee56e51f96f2fd4e745
Jakub <kubasurdej@gmail.com>
2023-11-12 22:05:12 +0100

    feat: improve navbar to contain information based on user state

--- src/components/Common/AppNavbar/AppNavbar.tsx
+++ src/components/Common/AppNavbar/AppNavbar.tsx
@@ -1,19 +1,42 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { RootState } from "@store/store";
 
 export const AppNavbar: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user.user);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+            PlaceholderReactApp
+          </Link>
         </Typography>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/register">
-          Register
-        </Button>
+        <Box>
+          {!user && (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/register">
+                Register
+              </Button>
+            </>
+          )}
+          {user && (
+            <div>
+              <Button color="inherit" component={Link} to={`/users/${user.id}`}>
+                Profile
+              </Button>
+              <Button color="inherit" component={Link} to={`/todos/${user.id}`}>
+                Todos
+              </Button>
+            </div>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
