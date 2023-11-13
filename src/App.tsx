9e4edd6723a942fb202214b4e62b7396402cc3fb
Jakub <kubasurdej@gmail.com>
2023-11-13 21:24:14 +0100

    fix: move hash router to a valid file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { Box, Container } from "@mui/material";
 import { RegisterPage } from "./pages/Register/Register";
 import { Footer } from "./components/Common/Footer/Footer";
